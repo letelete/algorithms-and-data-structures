0d464071a9fac1941301fbb9e84c16239249f52e
letelete <brunokawka@gmail.com>
2020-02-25 06:36:02 +0100

    Learn about generating alphabet in C++, so replaced a naive version

--- codeforces/1311_div_3/C.cpp
+++ codeforces/1311_div_3/C.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 typedef long long int i64;
 template <typename T>
@@ -11,40 +10,38 @@ typedef std::vector<std::string> vstr;
 typedef std::vector<char> vchar;
 typedef std::pair<int, int> pint;
 
-int t, n, m, mlen;
+int t, n, m;
 
 int main() {
   std::cin.tie(0);
   std::ios::sync_with_stdio(0);
 
   std::cin >> t;
+
   while (t--) {
-    std::map<char, int> occurs;
-    str alpha = "abcdefghijklmnopqrstuvwxyz";
-    for (const char& y : alpha) occurs[y] = 0;
-    std::cin >> n >> m;
     str s;
-    s.reserve(n);
+    std::map<char, int> occurs;
+    for (int i = 'a'; i <= 'z'; ++i) occurs[char(i)] = 0;
+    int prev = 0, index = 0;
+
+    std::cin >> n >> m;
     std::cin >> s;
-    int prev = 0;
     vint reps(m);
     for (int& i : reps) std::cin >> i;
+
     std::sort(reps.begin(), reps.end());
-    int index = 0;
     while (m > 0) {
       int right_excl = reps[index];
       for (int i = prev; i < right_excl; ++i) {
         occurs[s[i]] += m;
       }
-
       prev = right_excl;
-
       --m;
       ++index;
     }
-    for (int i = 0; i < s.length(); ++i) {
-      occurs[s[i]]++;
-    }
+
+    for (const char& y : s) occurs[y]++;
+
     for (const auto& p : occurs) std::cout << p.second << " ";
     std::cout << "\n";
   }
